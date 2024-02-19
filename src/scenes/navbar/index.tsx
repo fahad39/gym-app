c4246cf739443edc52675fc004ee7b742266d1a9
fahad39 <fahadhussain0127@gmail.com>
2024-02-19 22:27:39 +0500

    added nav bar

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -1,10 +1,37 @@
 import {useState} from "react"
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid"
+import Logo from "@/assets/Logo.png"
+
 
 type Props = {}
 
 const Navbar = (props: Props) => {
+  const flexBetween="flex items-center justify-between"
   return (
-    <div>Navbar</div>
+   <nav>
+    <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
+      <div className={`${flexBetween} mx-auto w-5/6`}>
+        <div className={`${flexBetween} w-full gap-16`}>
+          {/* left side */}
+          <img alt="logo" src={Logo} />
+          {/* right side */}
+          <div className={`${flexBetween} w-full`}>
+            <div className={`${flexBetween} gap-8 text-sm`}>
+              <p>Home</p>
+              <p>Benefits</p>
+              <p>Our Classes</p>
+              <p>Contat Us</p>
+            </div>
+            <div className={`${flexBetween} gap-8`}>
+              <p>Sign In</p>
+              <button>Become a Member</button>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+   </nav>
   )
 }
 
