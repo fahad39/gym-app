d881e5255f6698190800a6f15b9711903c864d17
fahad39 <fahadhussain0127@gmail.com>
2024-03-14 10:09:33 +0500

    classes page completed

--- src/scenes/ourClasses/index.tsx
+++ src/scenes/ourClasses/index.tsx
@@ -1,11 +1,43 @@
-import { SelectedPage } from '@/shared/types'
+import { ClassType, SelectedPage } from '@/shared/types'
 import image3 from "@/assets/image3.png"
 import image4 from "@/assets/image4.png"
 import image5 from "@/assets/image5.png"
 import image6 from "@/assets/image6.png"
 import { motion } from 'framer-motion'
 import Htext from '@/shared/Htext'
+import Class from './Class'
 
+
+const classes: Array<ClassType>=[
+    {
+        name:"Weight Training Classes",
+        description:"There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden",
+        image:image3
+    },
+    {
+        name:"Yoga Classes",
+        image:image4
+    },
+    {
+        name:"Ab core Classes",
+        image:image5
+    },
+    {
+        name:"Adventure Classes",
+        description:"There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden",
+        image:image6
+    },
+    {
+        name:"Fitness Classes",
+        description:"There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden",
+        image:image3
+    },
+    {
+        name:"Training Classes",
+        description:"There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden",
+        image:image5
+    },
+]
 
 type Props = {
     setSelectedPage:(value:SelectedPage)=>void
@@ -39,6 +71,24 @@ const OurClasses = ({setSelectedPage}: Props) => {
                 </div>
                 
             </motion.div>
+            <div
+                className='mt-10 h-[355px] w-full overflow-x-auto overflow-y-hidden'
+            >
+                <ul
+                    className='w-[28000px] whitespace-nowrap'
+                >
+                    {
+                        classes.map((item:ClassType,index)=>(
+                            <Class   
+                                key={`${item.name}-${index}`}
+                                name={item.name}
+                                description={item.description}
+                                image={item.image}
+                            />
+                        ))
+                    }
+                </ul>
+            </div>
         </motion.div>
 
     </section>
