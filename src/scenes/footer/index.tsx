a3c8973fdc712714f513178a1438ca78c41ad601
fahad39 <fahadhussain0127@gmail.com>
2024-03-26 06:54:27 +0500

    completed contact us

--- src/scenes/footer/index.tsx
+++ src/scenes/footer/index.tsx
@@ -21,6 +21,16 @@ const Footer = (props: Props) => {
                 className="mt-16 basis-1/4 md:mt-0"
             >
                 <h4 className="font-bold">Links</h4>
+                <p className="my-5">Massa orcu senectus</p>
+                <p className="my-5">Et gravida id et</p>
+                <p>Ullanma faajehjh</p>
+            </div>
+            <div
+                className="mt-16 basis-1/4 md:mt-0"
+            >
+                <h4 className="font-bold">Contact Us</h4>
+                <p className="my-5">Tenous metu mattis risus</p>
+                <p>(333)425-6826</p>
             </div>
             <div></div>
         </div>
